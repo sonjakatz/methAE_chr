76ef5af62dd72ff557f78af0a85f2e2596593f4e
sonja_mobile <katz@lifeglimmer.com>
2022-06-16 13:25:08 +0200

    fixed l1_reg

--- train_AE_l1reg.py
+++ train_AE_l1reg.py
@@ -11,7 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from data.prepareData import prepareDataLoader_fromPkl, prepareDataLoader_fromTensor
 from models.autoencoder import normalAE
-from scripts.train_EarlyStopping import train_AE
+from scripts.train_EarlyStopping import train_AE, train_AE_l1reg
 
 def loadData(PATH_train, PATH_val, batch_size=64):
     train_dataset = prepareDataLoader_fromPkl(PATH_train, colName="Age")
